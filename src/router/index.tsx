2b633e9ffc51a84df33867fc09cc6c397a187337
Centeno Jose Antonio <jocecito182@gmail.com>
2024-03-13 15:40:25 -0300

    Delete not needed files

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,13 +3,23 @@ import Cart from "@/pages/Cart";
 import Home from "@/pages/Home";
 import { createBrowserRouter } from "react-router-dom";
 
-const router = createBrowserRouter([{
-    path: '/',
-    element: (<Layout><Home /></Layout>)
-},
-{
-    path: '/cart',
-    element: (<Layout><Cart /></Layout>)
-}])
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Layout>
+        <Home />
+      </Layout>
+    ),
+  },
+  {
+    path: "/cart",
+    element: (
+      <Layout>
+        <Cart />
+      </Layout>
+    ),
+  },
+]);
 
 export default router;
